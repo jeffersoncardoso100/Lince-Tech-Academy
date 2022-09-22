eb28c98ba871fdbc96c40910e64202d8414648a8
Jefferson Cardoso <tatianibonecher@gmail.com>
2022-09-22 17:08:06 -0300

    %OTIMIZAÇÃO%

--- Orientação a Objetos/EX-2.dart
+++ Orientação a Objetos/EX-2.dart
@@ -66,46 +66,43 @@ class Pessoa {
     _caloriasConsumidas = produto.calorias;
   }
 }
-
-
 num calorias() {
+  Random _aleatorio = Random();
+  int _caloriasInicias = _aleatorio.nextInt(3000); //Objetivo 4 Concluido.
+  num _calorias = _caloriasInicias + _caloriasConsumidas;
 
-    Random _aleatorio = Random();
-    int _caloriasInicias = _aleatorio.nextInt(3000); //Objetivo 4 Concluido.
-    num _calorias = _caloriasInicias + _caloriasConsumidas;
-
-    if (_calorias <= 500) {
-      caloriasStatus = Status.deficitExtremo;
-      print('Status: Deficit extremo de calorias.');
+  if (_calorias <= 500) {
+    caloriasStatus = Status.deficitExtremo;
+    print('Status: Deficit extremo de calorias.');
+  } else {
+    if (_calorias <= 1800) {
+      caloriasStatus = Status.deficitCalorias;
+      print('Status: Deficit de calorias.');
     } else {
-      if (_calorias <= 1800) {
-        caloriasStatus = Status.deficitCalorias;
-        print('Status: Deficit de calorias.');
+      if (_calorias <= 2500) {
+        caloriasStatus = Status.satisfeitoPessoa;
+        print('Status: Pessoa está satisfeita.');
       } else {
-        if (_calorias <= 2500) {
-          caloriasStatus = Status.satisfeitoPessoa;
-          print('Status: Pessoa está satisfeita.');
-        } else {
-          if (_calorias > 2500) {
-            caloriasStatus = Status.excessoCalorias;
-            print('Status: Excesso de calorias.');
-          }
+        if (_calorias > 2500) {
+          caloriasStatus = Status.excessoCalorias;
+          print('Status: Excesso de calorias.');
         }
       }
     }
-
-    return _calorias;
   }
 
-  void refeicoes(num _calorias, int refeicoes) {
-    print('Foram feitas $refeicoes refeições.');
-    if (caloriasStatus == Status.excessoCalorias ||
-        caloriasStatus == Status.satisfeitoPessoa) {
-      print('A pessoa teve refeições suficientes.');
-    }
-    if (caloriasStatus == Status.deficitCalorias ||
-        caloriasStatus == Status.deficitExtremo) {
-      print('A pessoa está precisando de mais refeições.');
-    }
+  return _calorias;
+}
+
+void refeicoes(num _calorias, int refeicoes) {
+  print('Foram feitas $refeicoes refeições.');
+  if (caloriasStatus == Status.excessoCalorias ||
+      caloriasStatus == Status.satisfeitoPessoa) {
+    print('A pessoa teve refeições suficientes.');
   }
+  if (caloriasStatus == Status.deficitCalorias ||
+      caloriasStatus == Status.deficitExtremo) {
+    print('A pessoa está precisando de mais refeições.');
+  }
+}
 
