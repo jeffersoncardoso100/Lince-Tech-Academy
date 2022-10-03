5c1af5098ee9f17006311e502685a6cca680a846
Jefferson Cardoso <tatianibonecher@gmail.com>
2022-10-03 15:05:00 -0300

    Refator2

--- Coleções,Dinamismo 3/EX-2.dart
+++ Coleções,Dinamismo 3/EX-2.dart
@@ -1,22 +1,62 @@
-import 'dart:collection';
-main(){
-  final carta = Queue<String>();
-  carta.addFirst("♧ PAUS");
-  carta.addFirst("♡ COPAS");
-  carta.addFirst("♤ ESPADAS");
-  carta.addFirst("♢ OUROS");
+import 'dart:math' as math;
 
 
-  for(int i=0;i< carta.length;) {
-    for(int i=0;i< carta.length;) {
-      if(carta.elementAt(i) == '♧' || carta.elementAt(i) != '♡'
-          || carta.elementAt(i) == '♤' || carta.elementAt(i) != '♢'){
-        print(carta);
-        carta.remove(carta.elementAt(i));
 
-      }
-    }
+main() {
+  Map<String, int> figurinhas = {
+    'Messi': 0,
+    'Philippe ': 1,
+    'Cristiano Ronaldo': 2,
+    'Neymar Jr': 3,
+    'Hazard ': 4,
+    'Kevin De Bruyne': 5,
+    'Oblak': 6,
+    'Van Dijk': 7,
+    'Mohamed Salah': 8,
+    'Modrić ': 9,
+    'Ter Stegen': 10,
+    'Mbappé.': 11,
+    'Sergio Ramos': 12,
+    'Sergio Aguero': 13,
+    'Chiellini': 14,
+    'Lewandowski ': 15,
+    'Kane ': 16,
+    'Kalidou Koulibaly': 17,
+    "N'Golo Kanté": 18,
+    'Alisson ': 19,
+    'Griezmann ': 20,
+  };
 
+  final random = math.Random();
+  Set<String> figurinhasSelecionadas= {};
+  Set<String> albumCompleto= {};
+  List<int> figOrder= [];
+  List<int> figOrderComplete= [];
+  randomKey(Map figurinhas) =>
+      figurinhas.values.elementAt(random.nextInt(figurinhas.length));
+  for(int i =0;i<4;i++){
+    figOrder.add(randomKey(figurinhas));
   }
+  figOrder.sort();
+  for(int i=0; i<figOrder.length;i++) {
+    figurinhasSelecionadas.add(figurinhas.keys.elementAt(figOrder[i]));
+  }
+  for(int i =0;i<100;i++){
+    figOrderComplete.add(randomKey(figurinhas));
+  }
+  figOrderComplete.sort();
+  for(int i=0; i<figOrderComplete.toSet().length;i++) {
+    print(i);
+    albumCompleto.add(figurinhas.keys.elementAt(figOrderComplete.toSet().elementAt(i)));
+    print("Este é o album completo, com poucas chances mas com muita programção \n$albumCompleto");
+  }
+  print(figurinhasSelecionadas);
+  PacoteDeFiguras pacote1 = PacoteDeFiguras(figurinhasSelecionadas);
 }
 
+class PacoteDeFiguras {
+
+  Set<String> figurinhasSelecionadas= {};
+
+  PacoteDeFiguras(this.figurinhasSelecionadas);
+}
---
+++ Coleções,Dinamismo 3/EX-4.dart
@@ -0,0 +1,59 @@
+import 'dart:math' as math;
+
+main() {
+  Map<String, int> figurinhas = {
+
+    'Neymar jr': 0,
+    'Pedro': 1,
+    'Raphinha': 2,
+    'Vinicius Jr': 3,
+    'Antonio': 4,
+    'Rodrigo': 5,
+    'Weverton': 6,
+    'Ederson': 7,
+    'Danilo': 8,
+    'Alex Telles': 9,
+    'Bremer': 10,
+    'Eder Militão': 11,
+    'Ibañez': 22,
+    'Everton Ribeiro': 13,
+    'Fred': 14,
+    'Lucas Paquetá': 15,
+    'Gabriel': 16,
+    'Jefferson': 17,
+    'Bruno': 18,
+    'Alysson': 19,
+  };
+
+  final random = math.Random();
+  Set<String> figurinhasSelecionadas = {};
+  Set<String> albumCompleto = {};
+  List<int> figOrder = [];
+  List<int> figOrderComplete = [];
+  randomKey(Map figurinhas) =>
+      figurinhas.values.elementAt(random.nextInt(figurinhas.length));
+  for (int i = 0; i < 4; i++) {
+    figOrder.add(randomKey(figurinhas));
+  }
+  figOrder.sort();
+  for (int i = 0; i < figOrder.length; i++) {
+    figurinhasSelecionadas.add(figurinhas.keys.elementAt(figOrder[i]));
+  }
+  for (int i = 0; i < 100; i++) {
+    figOrderComplete.add(randomKey(figurinhas));
+  }
+  figOrderComplete.sort();
+  for (int i = 0; i < figOrderComplete.toSet().length; i++) {
+    print(i);
+    albumCompleto
+        .add(figurinhas.keys.elementAt(figOrderComplete.toSet().elementAt(i)));
+    print("$albumCompleto");
+  }
+
+}
+
+class PacoteDeFiguras {
+  Set<String> figurinhasSelecionadas = {};
+
+  PacoteDeFiguras(this.figurinhasSelecionadas);
+}
