b17c052737668b32692114a848ae49c8049c8e22
Jefferson Cardoso <tatianibonecher@gmail.com>
2022-10-20 11:35:34 -0300

    PrimeiraVersão

--- Calculadora_IMC_flutter/CalculadoraIMC.dart
+++ Calculadora_IMC_flutter/CalculadoraIMC.dart
@@ -14,36 +14,28 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-Widget buildTextField(String label){
+  Widget buildTextField(String label) {
     return TextField(
-
-
-
-
       decoration: InputDecoration(
-        fillColor: Colors.white38,
-         filled: true,
+          fillColor: Colors.white38,
+          filled: true,
           labelText: label,
-          labelStyle:
-          TextStyle(color: Colors.black45, fontSize: 30.0),
+          labelStyle: TextStyle(color: Colors.black45, fontSize: 30.0),
           border: OutlineInputBorder()),
       style: TextStyle(color: Colors.lightGreen, fontSize: 25.0),
       keyboardType: TextInputType.number,
-    )
-  ;
-
+    );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
         appBar: AppBar(
             backgroundColor: Colors.lightGreen,
             title: Text("Calculadora IMC"),
             centerTitle: true,
             actions: <Widget>[
               Container(
-
                 child: IconButton(
                   icon: Icon(Icons.refresh),
                   onPressed: () {},
@@ -55,49 +47,39 @@ Widget buildTextField(String label){
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-
-
               Icon(
-
-
                 Icons.person_pin,
                 size: 200.0,
                 color: Colors.lightGreen,
               ),
-
-
-             buildTextField('Reais'),
-
+              buildTextField('Reais'),
               Divider(),
               buildTextField('Altura'),
-             SizedBox(height: 15,),
-
+              SizedBox(
+                height: 15,
+              ),
               Container(
                 width: 100,
                 height: 50,
-                decoration: BoxDecoration (
-                  borderRadius: BorderRadius.circular(200),
-                  color:Colors.green,
-                  boxShadow: [
-                    BoxShadow(
-                      color:Colors.black45,
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(200),
+                    color: Colors.green,
+                    boxShadow: [
+                      BoxShadow(
+                          color: Colors.black45,
                           blurRadius: 10,
-                      spreadRadius: 2,
-                      offset: Offset(8,8)
-
-                    )
-                  ]
-                ),
+                          spreadRadius: 2,
+                          offset: Offset(8, 8))
+                    ]),
                 child: RaisedButton(
-                  child: Text("Verificar", style: TextStyle(color: Colors.white, fontSize: 25.0),),
-                  onPressed: (){},
+                  child: Text(
+                    "Verificar",
+                    style: TextStyle(color: Colors.white, fontSize: 25.0),
+                  ),
+                  onPressed: () {},
                   color: Colors.lightGreen,
-
                 ),
               )
-
-
-
             ],
           ),
         ));
