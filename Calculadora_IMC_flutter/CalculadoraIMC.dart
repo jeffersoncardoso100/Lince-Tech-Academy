e5408e4928ab8b6c193999e19f97d5c0a92fdcc4
Jefferson Cardoso <tatianibonecher@gmail.com>
2022-10-24 11:26:20 -0300

    CalculadoraFinalizada

--- Calculadora_IMC_flutter/CalculadoraIMC.dart
+++ Calculadora_IMC_flutter/CalculadoraIMC.dart
@@ -14,31 +14,69 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-  Widget buildTextField(String label) {
+  String infoText = 'Informe seus dados';
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+
+  Widget buildTextField(String label, TextEditingController c) {
     return TextField(
       decoration: InputDecoration(
           fillColor: Colors.white38,
           filled: true,
           labelText: label,
-          labelStyle: TextStyle(color: Colors.black45, fontSize: 30.0),
+          labelStyle: TextStyle(color: Colors.white54, fontSize: 30.0),
           border: OutlineInputBorder()),
-      style: TextStyle(color: Colors.lightGreen, fontSize: 25.0),
+      style: TextStyle(color: Colors.white, fontSize: 25.0),
       keyboardType: TextInputType.number,
+      controller: c,
     );
+  }
+
+  void _resetFields() {
+    setState(() {
+      weightController.text = "";
+      heightController.text = "";
+      infoText = "Informe seus dados";
+    });
+  }
+
+  void _calculate() {
+    double weight = double.parse(weightController.text);
+    double height = double.parse(heightController.text) / 100;
+    double imc = weight / (height * height);
+    setState(() {
+      if (imc < 17) {
+        infoText = "Muito abaixo do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 17 && imc <= 18.49) {
+        infoText = "Abaixo do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 15.5 && imc <= 24.99) {
+        infoText = "Peso ideal ou normal(${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 25 && imc <= 29.99) {
+        infoText = "Acima do peso (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 30 && imc <= 34.99) {
+        infoText = "Obesidade I (${imc.toStringAsPrecision(4)})";
+      } else if (imc >= 35 && imc <= 39.99) {
+        infoText = "Obesidade II (severa) (${imc.toStringAsPrecision(4)})";
+      } else if (imc > 40) {
+        infoText = "Obesidade III (morbida) (${imc.toStringAsPrecision(4)})";
+      }
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-            backgroundColor: Colors.lightGreen,
+            backgroundColor: Colors.black26,
             title: Text("Calculadora IMC"),
             centerTitle: true,
             actions: <Widget>[
               Container(
                 child: IconButton(
                   icon: Icon(Icons.refresh),
-                  onPressed: () {},
+                  onPressed: () {
+                    _resetFields();
+                  },
                 ),
               )
             ]),
@@ -47,14 +85,23 @@ class _HomeState extends State<Home> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
-              Icon(
-                Icons.person_pin,
-                size: 200.0,
-                color: Colors.lightGreen,
+              Container(
+                decoration: BoxDecoration(boxShadow: [
+                  BoxShadow(
+                      color: Colors.black87,
+                      blurRadius: 1000,
+                      spreadRadius: 250,
+                      offset: Offset(8, 8))
+                ]),
+                child: Icon(
+                  Icons.person_pin,
+                  size: 200.0,
+                  color: Colors.orange,
+                ),
               ),
-              buildTextField('Reais'),
+              buildTextField('Peso', weightController),
               Divider(),
-              buildTextField('Altura'),
+              buildTextField('Altura', heightController),
               SizedBox(
                 height: 15,
               ),
@@ -63,21 +110,32 @@ class _HomeState extends State<Home> {
                 height: 50,
                 decoration: BoxDecoration(
                     borderRadius: BorderRadius.circular(200),
-                    color: Colors.green,
+                    color: Colors.white54,
                     boxShadow: [
                       BoxShadow(
-                          color: Colors.black45,
-                          blurRadius: 10,
+                          color: Colors.white,
+                          blurRadius: 21,
                           spreadRadius: 2,
-                          offset: Offset(8, 8))
+                          offset: Offset(4, 4))
                     ]),
                 child: RaisedButton(
                   child: Text(
                     "Verificar",
                     style: TextStyle(color: Colors.white, fontSize: 25.0),
                   ),
-                  onPressed: () {},
-                  color: Colors.lightGreen,
+                  onPressed: () {
+                    _calculate();
+                  },
+                  color: Colors.orange,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(top: 45.0),
+                child: Text(
+                  infoText,
+                  style:
+                  TextStyle(color: Colors.deepOrange[400], fontSize: 30.0),
+                  textAlign: TextAlign.center,
                 ),
               )
             ],
